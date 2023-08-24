578c1260915acb271ee406361f773515dee12d6f
Dante-cpu <128062425+Dante-cpu@users.noreply.github.com>
2023-08-24 12:45:06 +0300

    Update ip_whois.sh

--- ip_whois.sh
+++ ip_whois.sh
@@ -2,7 +2,7 @@
 
 ip_file="ip_adresses.txt"
 
-while IFS=read -r ip_address; do
+while IFS= read -r ip_address; do
   whois_result=$(whois "$ip_address")
   org=$(echo "$whois_result" | grep -E '^org:' | awk -F ': ' '{print $2}')
   echo "$org" >>org.txt
